5deec4b5095a3852b3749523e36771aed5d1f451
Miroslav Saracevic <msaracevic@users.noreply.github.com>
2018-01-23 23:40:00 +0100

    Simplify resources page by splitting render in more functions

--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -8,57 +8,59 @@ export default class Resources extends Component {
     super(props);
     this.state = resources;
 
-    this.toggleVisible = this.toggleVisible.bind(this);
+    this.toggleVisibility       = this.toggleVisibility.bind(this);
+    this.toggleVisibilityButton = this.toggleVisibilityButton.bind(this);
   };
+
+  fillGistResponseData(key, response) {
+    let updated = Object.assign({}, this.state[key]);
+
+    updated.visible = false;
+    updated.content = ReactMarkdown({
+      source:    response,
+      className: 'gist'
+    });
+    this.setState({[key]: updated});
+  }
 
   componentWillMount() {
     Object.keys(this.state).forEach(key => {
-        if (this.state[key].gist) {
-          fetch(this.state[key].url, {'Content-Type': 'text/html'})
-            .then(response => response.text())
-            .then(response => {
-              let updated     = Object.assign({}, this.state[key]);
-              updated.visible = false;
-              updated.content = ReactMarkdown({
-                source:    response,
-                className: 'gist'
-              });
-              this.setState({
-                [key]: updated
-              });
-            });
-        }
+      if (this.state[key].gist) {
+        fetch(this.state[key].url, {'Content-Type': 'text/html'})
+          .then(response => response.text())
+          .then(response => this.fillGistResponseData(key, response));
       }
-    );
-  }
-
-  toggleVisible(key) {
-    let node     = Object.assign({}, this.state[key]);
-    node.visible = !node.visible;
-    this.setState({
-      [key]: node
     });
   }
 
-  resource() {
-    return Object.keys(this.state).map(key => {
-        if (this.state[key].gist) {
-          return (
-            <article className="resource" key={key}>
-              <h4>{this.state[key].title}</h4>
-              <p>
-                {this.state[key].description}
-                <button className="btn btn--link btn--small" onClick={() => this.toggleVisible(key)}>
-                  {(this.state[key].visible ? '(Show less...)' : '(Show more...)')}
-                </button>
-              </p>
-              <section className={'resource__gist ' + (this.state[key].visible ? '' : 'resource__gist--hidden')}>
-                {this.state[key].content ? this.state[key].content : ''}
-              </section>
-            </article>
-          );
-        } else return false;
-      }
+  toggleVisibility(key) {
+    let node = Object.assign({}, this.state[key]);
+
+    node.visible = !node.visible;
+    this.setState({[key]: node});
+  }
+
+  toggleVisibilityButton(key, visible) {
+    return (
+      <button className="btn btn--link btn--small" onClick={() => this.toggleVisibility(key)}>
+        {(visible ? '(Show less...)' : '(Show more...)')}
+      </button>
+    );
+  }
+
+  gistResource(key) {
+    const item = this.state[key];
+    return (
+      <article className="resource" key={key}>
+        <h4>{item.title}</h4>
+        <p>
+          {item.description}
+          {this.toggleVisibilityButton(key, item.visible)}
+        </p>
+        <section className={'resource__gist ' + (item.visible ? '' : 'resource__gist--hidden')}>
+          {item.content ? item.content : ''}
+        </section>
+      </article>
     );
   }
 
@@ -66,7 +68,10 @@ export default class Resources extends Component {
     return (
       <article className="resources">
         <h1>Useful resources</h1>
-        {this.resource()}
+        {Object.keys(this.state).map(key => {
+          if (this.state[key].gist) return this.gistResource(key);
+          else return (<span>This type of resource is not defined</span>);
+        })}
       </article>
     );
   }
