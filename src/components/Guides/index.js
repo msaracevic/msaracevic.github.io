c079595f9f33c40495ae8581abfc53b2177e38d7
Miroslav Saracevic <msaracevic@users.noreply.github.com>
2018-04-25 17:05:14 +0200

    Use embed gist as npm package

--- src/components/Guides/index.js
+++ src/components/Guides/index.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import EmbeddedLoader from './EmbeddedLoader';
+import ReactEmbedGist from 'react-embed-gist'
 
 export default class Guides extends Component {
   render() {
     return (
       <article className="guides">
-        <EmbeddedLoader gist="msaracevic/7a315f610c11d39aeea55061d3b9bf05"/>
-        <EmbeddedLoader gist="msaracevic/5d757e2fc72482a9a4a439969500c2eb"/>
+        <ReactEmbedGist gist="msaracevic/7a315f610c11d39aeea55061d3b9bf05"/>
+        <ReactEmbedGist gist="msaracevic/5d757e2fc72482a9a4a439969500c2eb"/>
       </article>
     );
   }
