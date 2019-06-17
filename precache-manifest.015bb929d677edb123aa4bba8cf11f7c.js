89588a944dbd681f6fd7f4ec38803f80ff6a0c42
Miroslav Saracevic <msaracevic@users.noreply.github.com>
2019-06-17 14:15:55 +0200

    Release

---
+++ precache-manifest.015bb929d677edb123aa4bba8cf11f7c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb620f3d29992fce1458c0922a42e708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e7114c27848257c32c9",
+    "url": "/static/js/2.ee037810.chunk.js"
+  },
+  {
+    "revision": "60950a2b22ac374318bb",
+    "url": "/static/js/main.79fd7bef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
